6116740f4c4cca4f07e9fc9338f4f46480dc8a92
Laurene-Ipsum <laureneipsum@gmail.com>
2020-12-17 23:07:01 +0100

    🥁 Day 1 - Drum Kit

---
+++ Day1_Drum_Kit/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('keydown',function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key =  document.querySelector(`.key[data-key="${e.keyCode}"]`);
+ if (!audio) return; // stop la fonction
+  audio.currentTime = 0; //  refaire start direct sans attendre la fin du son
+  audio.play();
+  key.classList.add('playing');
+});
+
+
+function removeTransition(e) {
+   if(e.propertyName !== 'transform') return; // skip
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
